9a2c9c97d39efc357f7054bf36fc233085da6d91
Sarang <sarang@clayfin.com>
2022-06-17 15:26:12 +0530

    ...

---
+++ sample1/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Sample from './Components/Sample'
+
+const App = () => {
+  return (
+    <View>
+      <Sample /> 
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ sample1/Components/Sample.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Sample = () => {
+  return (
+    <View>
+      <Text>Sample 1
+      </Text>
+    </View>
+  )
+}
+
+export default Sample
+
+const styles = StyleSheet.create({})
